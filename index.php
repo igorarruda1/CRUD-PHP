38b5d2c24590b7a0e43847649636f3afe327f917
Igorarruda1 <srigaoarruda@gmail.com>
2021-06-07 16:05:00 -0300

    Mudando para o PHP

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela 1</title>
+</head>
+<body>
+        <h1>CRUD - PHP</h1>
+    <label for="fname">Nome: </label>
+    <input type="text" placeholder="Digite seu nome"></input><br><br>
+    <label for="fpass">Senha: </label>
+    <input type="password"  placeholder="Digite sua senha"></input> <br><br>
+
+    <button type="submit">CADASTRAR</button>
+    <button type="submit" value="submit">LOGIN</button>
+
+    
+</body>
+</html>
