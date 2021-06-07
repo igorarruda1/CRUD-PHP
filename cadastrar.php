945864b9860597521966087d377972b7faa95fc8
Igorarruda1 <srigaoarruda@gmail.com>
2021-06-07 17:00:11 -0300

    Conexao com o Banco

--- cadastrar.php
+++ cadastrar.php
@@ -4,18 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tela 1</title>
+    <title>Tela de Cadastro</title>
 </head>
 <body>
-        <h1>CRUD - PHP</h1>
+        <h1>Tela de Cadastramento</h1>
     <label for="fname">Nome: </label>
     <input type="text" placeholder="Digite seu nome"></input><br><br>
     <label for="fpass">Senha: </label>
     <input type="password"  placeholder="Digite sua senha"></input> <br><br>
+    <label for="Fmail">E-mail: </label>
+    <input type="email"  placeholder="Digite seu E-mail"></input> <br><br>
 
-    <a href="cadastrar.php">CADASTRO</a>
-    <button type="submit" value="submit">LOGIN</button>
-
-    
+    <button type="submit">CADASTRAR</button>
+    <a href="index.php">VOLTAR</a>
 </body>
 </html>
---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+
+$host =  "localhost";
+$user = "root";
+$pass = "";
+$bd = "crud-php";
+
+// conexao com o bando de dados 
+$mysqli = new mysqli($host, $user, $pass, $bd);
+
+if($mysqli -> connect_errno)
+    echo "Falha na conexão: ( " .$mysqli-> connect_errno . ") " . $mysqli->connect_error;
+
+?> 
