c4438d81d2ee3910c8cb819ef5f9c12587dbf979
Igorarruda1 <srigaoarruda@gmail.com>
2021-06-08 19:05:13 -0300

    configurando login

--- conexao.php
+++ conexao.php
@@ -11,4 +11,8 @@ $mysqli = new mysqli($host, $user, $pass, $bd);
 if($mysqli -> connect_errno)
     echo "Falha na conexão: ( " .$mysqli-> connect_errno . ") " . $mysqli->connect_error;
 
+
+
+    // Create connection
+$conn = mysqli_connect($host, $user, $pass, $bd);
 ?> 
--- login.php
+++ login.php
@@ -1,40 +1,12 @@
-<?php include 'conexao.php'; ?>
-
-<?php  
-
-
-
-
-// Create connection
-$conn = mysqli_connect($host, $user, $pass, $bd);
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-
-$_mail = $_POST["mail"];
-$_pass = $_POST["pass"];
-
-  // Selecionar apenas o usuario do email
- $sql = "SELECT * FROM user WHERE ('$_mail' = user_mail and '$_pass' = user_pass)";
- $result = $conn->query($sql);
-
- if($result-> num_rows > 0) {
-   // saida de cada linha da tabela
-    while($linha = $result-> fetch_assoc()){
-      echo 'ID: ' , $linha['id_user'],'<BR>';
-      echo 'NOME: ', $linha['user_name'],'<br>';
-      echo 'EMAIL: ', $linha['user_mail'],'<br>';
-    }
-
-  
-
- }else{
-  echo  '<script>alert("Email ou senha incorreta!");</script>';
-    
- }
-
-
-
-mysqli_close($conn);
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+BEM VINDO
+</body>
+</html>
