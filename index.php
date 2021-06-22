38865537639a8e87ad77471bd1c34b0f71017e42
Igorarruda1 <srigaoarruda@gmail.com>
2021-06-22 13:49:25 -0300

    logout user

--- index.php
+++ index.php
@@ -1,36 +1,31 @@
+<?php 
+ require 'conexao.php';
+
+ if(isset($_SESSION['idUser']) && !empty($_SESSION['idUser'])):
+ 
+?>
 
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
-    <title>Tela 1</title>
-
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CRUD</title>
 </head>
 <body>
-        <div class="login">
-        <h1>CRUD - PHP</h1>
-
-    <form action="logar.php" method="POST">
-    <label for="fname">E-mail: </label>
-    <input type="text" placeholder="Digite E-mail"  name="mail"></input><br>
-    <label for="fpass">Senha: </label>
-    <input type="password"  placeholder="Digite sua senha"  name="pass" id="pass"></input> <br>
 
 
-    <button type="submit" value="submit" >LOGIN</button>
-  
-    </form>
-    <a href="cadastrar.php">CADASTRO</a>
-    </div>
+
+BEM VINDO
+
+<a href="logout.php">SAIR</a>
 </body>
 </html>
 
+<?php  
 
+ else: header("location: login.php"); endif;
 
-
-
-
+?>
--- login.php
+++ login.php
@@ -1,15 +1,36 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+    <title>Tela 1</title>
+
 </head>
 <body>
+        <div class="login">
+        <h1>CRUD - PHP</h1>
+
+    <form action="logar.php" method="POST">
+    <label for="fname">E-mail: </label>
+    <input type="text" placeholder="Digite E-mail"  name="mail"></input><br>
+    <label for="fpass">Senha: </label>
+    <input type="password"  placeholder="Digite sua senha"  name="pass" id="pass"></input> <br>
 
 
-
-BEM VINDO
+    <button type="submit" value="submit" >LOGIN</button>
+  
+    </form>
+    <a href="cadastrar.php">CADASTRO</a>
+    </div>
 </body>
 </html>
+
+
+
+
+
+
