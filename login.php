3404e9ade14ed4486b15a318293259153f358a9d
Igorarruda1 <srigaoarruda@gmail.com>
2021-06-23 17:44:42 -0300

    Inicializando Bootstraps

--- login.php
+++ login.php
@@ -1,32 +1,42 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Tela 1</title>
 
 </head>
 <body>
-        <div class="login">
+        <div class="container">
         <h1>CRUD - PHP</h1>
 
     <form action="logar.php" method="POST">
-    <label for="fname">E-mail: </label>
-    <input type="text" placeholder="Digite E-mail"  name="mail"></input><br>
-    <label for="fpass">Senha: </label>
-    <input type="password"  placeholder="Digite sua senha"  name="pass" id="pass"></input> <br>
-
-
-    <button type="submit" value="submit" >LOGIN</button>
-  
-    </form>
-    <a href="cadastrar.php">CADASTRO</a>
+    <div class="col-md-3 form-group">
+        <label for="fname" class="form-label">E-mail: </label>
+        <input type="text" class="form-control"  name="mail" placeholder="name@example.com">
     </div>
+    <div class="col-md-3 form-group">
+        <label for="fpass" class="form-label">Senha: </label>
+        <input type="password" class="form-control" name="pass"  placeholder="Digite sua senha">
+    </div>
+
+    <button type="submit"  class="btn btn-primary">LOGIN</button>
+
+
+    <a href="cadastrar.php">CADASTRO</a>
+    </form>
+    
+    </div>
+
+    
+
 </body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </html>
 
 
